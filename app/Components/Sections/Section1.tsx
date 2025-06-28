f24d39e11112fb55791053edb252859a0f401489
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-28 22:52:59 +0530

    Finished the TwoDCard

--- app/Components/Sections/Section1.tsx
+++ app/Components/Sections/Section1.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { hover, motion } from 'motion/react'
+import { motion } from 'motion/react'
 
 const Section1: React.FC<{classname: string}> = ({classname}) => {
   return (
@@ -9,41 +9,84 @@ const Section1: React.FC<{classname: string}> = ({classname}) => {
             <span>Scroll Animation</span>
         </h1>
         <div className='bg-red-300 flex-1 m-6'>
-            <motion.div
-                whileHover="hover"
+            <TwoDCard />
+        </div>
+    </section>
+  )
+}
+
+const TwoDCard = () => {
+    return (
+        <motion.div
+            whileHover="hover"
+            variants={{
+                hover: {
+                    scale: 1.05,
+                },
+            }} 
+            transition={{
+                duration: 1,
+                ease: "backInOut",
+            }}
+            className='relative h-96 w-80 overflow-hidden bg-indigo-500 rounded-xl cursor-pointer text-white p-8 flex flex-col justify-between'
+        >
+            <span className='z-1 relative'>Pro</span>
+            <motion.span 
+                initial={{ scale: 0.85 }}
                 variants={{
                     hover: {
-                        scale: 1.05,
+                        scale: 1.1,
                     },
-                }} 
+                }}
                 transition={{
                     duration: 1,
                     ease: "backInOut",
                 }}
-                className='h-96 w-80 overflow-hidden bg-indigo-500 rounded-xl cursor-pointer text-white p-8 flex flex-col justify-between'
+                className='z-1 text-6xl font-bold origin-top-left'
             >
-                <span>Pro</span>
-                <motion.span 
-                    initial={{ scale: 0.85 }}
-                    variants={{
-                        hover: {
-                            scale: 1,
-                        },
-                    }}
-                    transition={{
-                        duration: 1,
-                        ease: "backInOut",
-                    }}
-                    className='text-6xl font-bold origin-top-left'
-                >
-                    $299/Month
-                </motion.span>
-                <span>Lorem Ipsum dolor sit amet sonsecteutur adipisicing elit.</span>
-                <button className='w-full bg-white text-black font-black p-2 rounded-sm'>GET IT NOW</button>
-            </motion.div>
-        </div>
-    </section>
-  )
+                $299/Month
+            </motion.span>
+            <span className='z-1 '>Lorem Ipsum dolor sit amet sonsecteutur adipisicing elit.</span>
+            <button className='z-1 w-full bg-white text-black font-black p-2 rounded-sm'>GET IT NOW</button>
+            <CardBackground />
+        </motion.div>
+    )
+}
+
+const CardBackground = () => {
+    return (
+        <motion.svg 
+            width="320" 
+            height="384" 
+            viewBox="0 0 320 384" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute inset-0 z-0"
+            variants={{
+                hover: {
+                    scale: 1.5,
+                },
+            }}
+            transition={{
+                duration: 1,
+                ease: "backInOut"
+            }}
+        >
+            <motion.circle 
+                cx="160.5" 
+                cy="125.5" 
+                r="89.5" 
+                fill="black"
+            />
+            <motion.ellipse 
+                cx="160.5" 
+                cy="277.5" 
+                rx="89.5" 
+                ry="36.5" 
+                fill="black"
+            />
+        </motion.svg>
+    )
 }
 
 export default Section1
