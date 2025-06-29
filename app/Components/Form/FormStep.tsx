9f2b762471b4bb980491ca4704dfdfd0319bdbf4
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-29 18:13:24 +0530

    Minor changes

--- app/Components/Form/FormStep.tsx
+++ app/Components/Form/FormStep.tsx
@@ -30,22 +30,4 @@ const FormStep = () => {
   )
 }
 
-// return (
-//     <motion.div 
-//         className='w-16 h-16 bg-blue-200 rounded-full flex justify-center items-center'
-//         variants={{
-//             start: { rotate: 0 },
-//             finish: { rotate: 360 }
-//         }}
-//         initial='start'
-//         animate='finish'
-//         transition={{ duration: 2, repeat: Infinity }}
-//     >
-//         <div className='w-12 h-12 bg-blue-700 rounded-full flex justify-center items-center'>
-//             <span >1</span>
-//             <span className='hidden'>2</span>
-//         </div>
-//     </motion.div>
-//   )
-
 export default FormStep
