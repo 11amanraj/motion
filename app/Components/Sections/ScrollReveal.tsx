aff4efae4120cc6e37dcf4d8e52f9d63c0f32b3b
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-29 15:30:06 +0530

    Created Loading component

--- app/Components/Sections/ScrollReveal.tsx
+++ app/Components/Sections/ScrollReveal.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import Loading from '../Loading'
 
 const ScrollReveal = () => {
   return (
     <section className='bg-green-400 h-screen w-full flex flex-col'>
         <h1 className='text-7xl m-6 font-mono font-bold'>
-            <span>002 </span>
-            <span>Scroll Reveal</span>
+            <Loading>
+                <span>002 </span>
+                <span>Scroll Reveal</span>
+            </Loading>
         </h1>
-        <div className='bg-red-300 flex-1 m-6 flex gap-12 justify-center items-center'>
-            Lorem Ipsum
+        <div className='bg-red-300 flex-1 m-6 flex flex-col gap-12 justify-center items-center'>
+            <h2 className='text-[120px]'>
+                <Loading>
+                    Lorem Ipsum
+                </Loading>
+            </h2>
+            <div className='text-6xl'>
+                <Loading>
+                    Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                </Loading>
+            </div>
+            <div className='text-2xl'>
+                <Loading>
+                    Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                </Loading>
+            </div>
         </div>
     </section>
   )
