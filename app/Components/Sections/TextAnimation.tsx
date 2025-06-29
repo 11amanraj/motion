da957c46d5ce824e282015d5e12447d23ec1b459
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-29 15:38:49 +0530

    Created TextAnimation component

---
+++ app/Components/Sections/TextAnimation.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TextAnimation = () => {
+  return (
+    <section className='bg-yellow-300 h-screen w-full flex flex-col'>
+        <h1 className='text-7xl m-6 font-mono font-bold'>
+            <span>003 </span>
+            <span>Text Animation</span>
+        </h1>
+        <div className='bg-red-300 flex-1 m-6 flex gap-12 justify-center items-center'>
+            Lorem Ipsum
+        </div>
+    </section>
+  )
+}
+
+export default TextAnimation
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Overview from "./Components/Overview";
 import Animation from "./Components/Animation";
 import CardsSection from "./Components/Sections/CardsSection";
 import ScrollReveal from "./Components/Sections/ScrollReveal";
+import TextAnimation from "./Components/Sections/TextAnimation";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Animation />
       <CardsSection classname="bg-blue-700"/>
       <ScrollReveal />
+      <TextAnimation />
     </div>
   );
 }
