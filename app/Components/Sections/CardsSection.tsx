fc05e783b20344bf2a030ac8ba6e5c06027651ad
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-29 14:00:50 +0530

    changed heading

--- app/Components/Sections/CardsSection.tsx
+++ app/Components/Sections/CardsSection.tsx
@@ -7,7 +7,7 @@ const CardsSection: React.FC<{classname: string}> = ({classname}) => {
     <section className={`${classname} h-screen w-full flex flex-col`}>
         <h1 className='text-7xl m-6'>
             <span>001 </span>
-            <span>Scroll Animation</span>
+            <span>Cards Animation</span>
         </h1>
         <div className='bg-red-300 flex-1 m-6 flex gap-12 justify-center items-center'>
             <TwoDCard />
