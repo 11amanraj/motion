2e132552f6d166b30a9eb16087521ba7cb6f17cc
11amanraj <110328977+11amanraj@users.noreply.github.com>
2025-06-29 16:07:45 +0530

    Removed unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import Overview from "./Components/Overview";
-import Animation from "./Components/Animation";
 import CardsSection from "./Components/Sections/CardsSection";
 import ScrollReveal from "./Components/Sections/ScrollReveal";
 import TextAnimation from "./Components/Sections/TextAnimation";
@@ -9,8 +7,6 @@ import TextAnimation from "./Components/Sections/TextAnimation";
 export default function Home() {
   return (
     <div>
-      <Overview />
-      <Animation />
       <CardsSection classname="bg-blue-700"/>
       <ScrollReveal />
       <TextAnimation />
